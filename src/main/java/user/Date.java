a034eaa83cfd5b8f34bfce03f150adf367ed2683
Öner Acet <onract@gmail.com>
2022-06-11 16:54:46 +0300

    -

--- src/main/java/user/Date.java
+++ src/main/java/user/Date.java
@@ -1,7 +1,14 @@
 package main.java.user;
 
 public class Date {
+    public String month;
+    public String day;
+    public String tim;
+
     public Date(){
 
+        String day;
+        String month;
+        String time;
     }
 }
